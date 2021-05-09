e01b9116b07cd0b786b90258ce6a159cfffc9f00
amitwaghmaretorinit <amit@torinit.ca>
2021-05-09 23:00:57 +0530

    home page added

--- Screens/HomeScreen/index.js
+++ Screens/HomeScreen/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 const HomeScreen = () => {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
+        <View style={{ flex: 1, }}>
+            <View style={{ backgroundColor: '#FFECD5', padding: 20, flexDirection: 'row', justifyContent: 'center' }}>
+                <Text style={{ marginLeft: 100 }}>WELCOME TO SEE KINDNESS</Text>
+                <View style={{ marginLeft: 40, marginRight: 20, flexDirection: 'row' }}>
+                    <Icon name="caret-down" size={20} />
+                    <Text style={{ marginLeft: 5 }}>More</Text>
+                </View>
+
+            </View>
         </View>
     )
 }
