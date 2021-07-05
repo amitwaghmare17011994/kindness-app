367b2820e4493b2bab99acd7772648c6bfb8e7b7
amit <amit@torinit.ca>
2021-07-05 12:08:23 +0530

    screen animation added

--- Navigators/index.js
+++ Navigators/index.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import { TransitionSpecs, HeaderStyleInterpolators } from '@react-navigation/stack';
+
 import HomeScreen from '../Screens/HomeScreen';
 import PostScreen from '../Screens/PostScreen';
 
@@ -17,6 +19,43 @@ import ColorChooser from '../Screens/ColorChooser';
 import DonationScreen from '../Screens/Donation';
 import CompleteDonation from '../Screens/Donation/CompleteDoation';
 
+const MyTransition = {
+  gestureDirection: 'horizontal',
+  transitionSpec: {
+    open: TransitionSpecs.TransitionIOSSpec,
+    close: TransitionSpecs.TransitionIOSSpec,
+  },
+  headerStyleInterpolator: HeaderStyleInterpolators.forFade,
+  cardStyleInterpolator: ({ current, next, layouts }) => {
+    return {
+      cardStyle: {
+        transform: [
+          {
+            translateX: current.progress.interpolate({
+              inputRange: [0, 1],
+              outputRange: [layouts.screen.width, 0],
+            }),
+          },
+           
+          {
+            scale: next
+              ? next.progress.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [1, 0.9],
+                })
+              : 1,
+          },
+        ],
+      },
+      overlayStyle: {
+        opacity: current.progress.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, 0.5],
+        }),
+      },
+    };
+  },
+}
 const Stack = createStackNavigator();
 function Navigator() {
   return (
@@ -24,70 +63,70 @@ function Navigator() {
       {/* <script source="http://localhost:8097"/> */}
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen
-          options={{headerShown: false}}
+          options={{headerShown: false, ...MyTransition}}
           name="Home"
           component={HomeScreen}
         />
         <Stack.Screen
-          options={{headerShown: false}}
+          options={{headerShown: false, ...MyTransition}}
           name="PostKindness"
           component={PostScreen}
         />
 
         <Stack.Screen
-          options={{headerShown: false}}
+          options={{headerShown: false, ...MyTransition}}
           name="PostLocationScreen"
           component={PostLocationScreen}
         />
         <Stack.Screen
-          options={{headerShown: false}}
+          options={{headerShown: false, ...MyTransition}}
           name="OurMissionScreen"
           component={OurMissionScreen}
         />
         <Stack.Screen
-          options={{headerShown: false}}
+          options={{headerShown: false, ...MyTransition}}
           name="LearnMoreScreen"
           component={LearnMoreScreen}
         />
         <Stack.Screen
-          options={{headerShown: false}}
+          options={{headerShown: false, ...MyTransition}}
           name="TeamScreen"
           component={TeamScreen}
         />
         <Stack.Screen
-          options={{headerShown: false}}
+          options={{headerShown: false, ...MyTransition}}
           name="MagzineScreen"
           component={MagzineScreen}
         />
         <Stack.Screen
-          options={{headerShown: false}}
+          options={{headerShown: false, ...MyTransition}}
           name="ProfileScreen"
           component={ProfileScreen}
         />
 
         <Stack.Screen
-          options={{headerShown: false}}
+          options={{headerShown: false, ...MyTransition}}
           name="SubscribeScreen"
           component={SubscribeScreen}
         />
         <Stack.Screen
-          options={{headerShown: false}}
+          options={{headerShown: false, ...MyTransition}}
           name="BisooScreen"
           component={BisooScreen}
         />
 
         <Stack.Screen
-          options={{headerShown: false}}
+          options={{headerShown: false, ...MyTransition}}
           name="PurchaseBisooScreen"
           component={PurchaseBisooScreen}
         />
         <Stack.Screen
-          options={{headerShown: false}}
+          options={{headerShown: false, ...MyTransition}}
           name="ColorChooser"
           component={ColorChooser}
         />
          <Stack.Screen
-          options={{headerShown: false}}
+          options={{headerShown: false, ...MyTransition}}
           name="DonationScreen"
           component={DonationScreen}
         />
