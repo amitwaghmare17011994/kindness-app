ef2208ae985740da21447ee6f5fb126ab8d8d116
amit <amit@torinit.ca>
2021-07-12 14:49:44 +0530

    disabled state added

--- components/RoundButton/index.js
+++ components/RoundButton/index.js
@@ -1,28 +1,41 @@
-import React from 'react'
-import { Button } from 'native-base'
-import { Text, StyleSheet } from 'react-native'
+import React from 'react';
+import {Button} from 'native-base';
+import {Text, StyleSheet} from 'react-native';
 
-const RoundButton = (props) => {
-    const {onPress=()=>{},disabled=false, text, customStyles = {}, textStyles = {} } = props
-    return (
-        <Button disabled={disabled} onPress={onPress}  style={{ ...styles.postButton, ...customStyles}}>
-            { text && <Text style={{ ...textStyles }}>{text}</Text>
-            }
-            {props.children}
-        </Button>
-    )
-}
-
+const RoundButton = props => {
+  const {
+    onPress = () => {},
+    disabled = false,
+    text,
+    customStyles = {},
+    textStyles = {},
+  } = props;
+  return (
+    <Button
+      disabled={disabled}
+      onPress={onPress}
+      style={[
+        styles.postButton,
+        customStyles,
+        disabled?styles.disabled:{}
+      ]}>
+      {text && <Text style={{...textStyles}}>{text}</Text>}
+      {props.children}
+    </Button>
+  );
+};
 
 const styles = StyleSheet.create({
+  disabled: {
+    backgroundColor: 'gray',
+  },
+  postButton: {
+    // flex: 0.3,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignSelf: 'flex-end',
+  },
+});
 
-    postButton: {
-        // flex: 0.3,
-        backgroundColor: 'white',
-        borderRadius: 20,
-        justifyContent: 'center',
-        alignSelf: 'flex-end'
-    },
-})
-
-export default RoundButton
+export default RoundButton;
