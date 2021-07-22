fcc4afbf55fc2b844878156c40c38cbdb920dd1d
amit <amit@torinit.ca>
2021-07-22 14:55:33 +0530

    loader added

--- components/AppLayout/index.js
+++ components/AppLayout/index.js
@@ -1,30 +1,47 @@
 import React from 'react';
-import {View, StyleSheet, ScrollView} from 'react-native';
+import {View, StyleSheet, ScrollView, Text} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import AppFooter from '../AppFooter';
 import AppHeader from '../AppHeader';
-import {SCREEN_HEIGHT, MENU_ITEMS} from '../../constants';
+import {SCREEN_HEIGHT, MENU_ITEMS, SCREEN_WIDTH} from '../../constants';
+import {Spinner} from 'native-base';
+import {useSelector} from 'react-redux';
 
 const AppLayout = props => {
   const navigation = useNavigation();
+  const showLoader = useSelector(state => state.rawData.showLoader);
   const onMenuItemSelected = route => {
-      navigation.navigate(route);
+    navigation.navigate(route);
   };
-
-  return (
+   return (
     <View style={{flex: 1}}>
+      {showLoader && (
+        <View style={styles.loader}>
+          <View style={{marginTop: SCREEN_WIDTH}}>
+            <Spinner accessibilityLabel="Loading posts" />
+          </View>
+        </View>
+      )}
       <View elevation={5} style={[styles.square]}>
         <AppHeader onMenuItemSelected={onMenuItemSelected} />
       </View>
       <ScrollView style={{flex: 1, maxHeight: SCREEN_HEIGHT}}>
         {props.children}
-        <AppFooter onMenuItemSelected={onMenuItemSelected}/>
+        <AppFooter onMenuItemSelected={onMenuItemSelected} />
       </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  loader: {
+    top: 0,
+    backgroundColor: '#00000099',
+    position: 'absolute',
+    minHeight: SCREEN_HEIGHT * 50,
+    width: SCREEN_WIDTH,
+    zIndex: 111,
+  },
   container: {
     flex: 1,
     backgroundColor: '#ecf0f1',
@@ -38,8 +55,8 @@ const styles = StyleSheet.create({
   square: {
     backgroundColor: '#f8f9f8',
     // height: 0,
-    paddingBottom:10,
-    paddingTop:30,
+    paddingBottom: 10,
+    paddingTop: 30,
     shadowColor: 'black',
     display: 'flex',
     flexDirection: 'row',
--- services/request.js
+++ services/request.js
@@ -1,24 +1,36 @@
 import Axios, {AxiosResponse, AxiosError} from 'axios';
 import { END_POINT } from '../constants';
+import { updateRawData } from '../Reducers/actions';
 import {getServerURL, getRequestedHeader} from '../utils';
 
 export const  doGet = async (url) => {
     try {
+        updateRawData({showLoader:true})
         const {data} = await Axios.get(`${END_POINT}${url}`, getRequestedHeader())
+        updateRawData({showLoader:false})
 
         return data;
     } catch (error) {
+        updateRawData({showLoader:false})
+
         console.warn(error)
     }
 }
 
 export const doPost = (url, data) => {
+    updateRawData({showLoader:true})
 
     return Axios.post(`${END_POINT}${url}`, data, getRequestedHeader())
         .then((response) => {
+            updateRawData({showLoader:false})
+
             return response.data;
+ 
         })
         .catch((error) => {
+            updateRawData({showLoader:false})
+
             throw error;
+            
         });
 };
