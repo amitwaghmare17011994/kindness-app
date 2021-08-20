e7dcf3953850aad9b595d5aa414b230fa4163bb4
gajesh1 <panigrahigajesh@gmail.com>
2021-08-20 14:58:46 +0530

    Added like.

--- Screens/ProfileScreen/ProfileListItem.js
+++ Screens/ProfileScreen/ProfileListItem.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/Entypo';
 import InputField from '../../components/Input';
 import RoundButton from '../../components/RoundButton';
+import {useLike} from './../../hooks/useLike';
 
-const ProfileListItem = ({showPostForm=false, post = {}}) => {
+const ProfileListItem = ({
+  showPostForm = false,
+  post = {},
+  userDetails = {},
+}) => {
+  const {likeList, onLikeClicked} = useLike(post, userDetails);
+
   return (
     <View style={{paddingBottom: 10}}>
       <View style={styles.row}>
@@ -15,34 +22,37 @@ const ProfileListItem = ({showPostForm=false, post = {}}) => {
         <Text style={styles.blur}>15 mins ago</Text>
         <Icon name="dots-three-vertical" />
       </View>
-      <Text style={{fontWeight: '700', marginTop: 5}}>
-        {post.post_content}
-      </Text>
+      <Text style={{fontWeight: '700', marginTop: 5}}>{post.post_content}</Text>
       <View style={{flexDirection: 'row-reverse', marginTop: 5}}>
         <View style={{flexDirection: 'row', marginLeft: 20}}>
           <Icon name="chat" size={16} />
           <Text style={{fontSize: 12}}>185</Text>
         </View>
 
-        <View style={{flexDirection: 'row', marginLeft: 20}}>
-          <Icon name="heart" size={16} />
-        </View>
+        <TouchableOpacity onPress={() => onLikeClicked(likeList)}>
+          <View style={{flexDirection: 'row', marginLeft: 20}}>
+            <Icon name={likeList.includes(userDetails.id) ? "heart" : "heart-outlined"} size={16} />
+            <Text style={{fontSize: 12}}>{likeList?.length || 0}</Text>
+          </View>
+        </TouchableOpacity>
 
         <View style={{flexDirection: 'row'}}>
           <Icon name="arrow-long-up" size={16} />
           <Text style={{fontSize: 12}}>185</Text>
         </View>
       </View>
-      {showPostForm &&<>
-      <View style={{flexDirection: 'row',marginTop:10}}>
-        <InputField />
-      </View>
-      <View style={{flexDirection: 'row-reverse'}}>
-        <RoundButton customStyles={{height: 30, width: 128, marginTop: 10}}>
-          <Text>POST</Text>
-        </RoundButton>
-      </View>
-      </>}
+      {showPostForm && (
+        <>
+          <View style={{flexDirection: 'row', marginTop: 10}}>
+            <InputField />
+          </View>
+          <View style={{flexDirection: 'row-reverse'}}>
+            <RoundButton customStyles={{height: 30, width: 128, marginTop: 10}}>
+              <Text>POST</Text>
+            </RoundButton>
+          </View>
+        </>
+      )}
     </View>
   );
 };
